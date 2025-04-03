8be9f4b1fb381ed340c204f5dacd6ff8b8826bec
EmersonSales <46733478+EmersonSales@users.noreply.github.com>
2025-04-03 13:46:38 -0400

    Create InstalarAgentUbuntuServer.sh

---
+++ InstalarAgentUbuntuServer.sh
@@ -0,0 +1,3 @@
+wget https://github.com/glpi-project/glpi-agent/releases/download/1.13/glpi-agent-1.13-linux-installer.pl
+chmod +x glpi-agent-1.13-linux-installer.pl
+.\glpi-agent-1.13-linux-installer.pl --install --runnow
